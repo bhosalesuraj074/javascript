2ec5875662054b0b36f6278c92ad49542f88867f
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-02-18 18:23:35 +0530

    Create 01_typeOf.js

---
+++ 01_typeOf.js
@@ -0,0 +1,12 @@
+var bankName;
+console.log("before inilization bankName "+typeof bankName);
+bankName="SBI Bank";
+console.log("bankName datatype: "+typeof bankName);
+var marks='90%';
+console.log("Marks datatype: "+typeof marks);
+var isWorking;
+console.log("isWorking datatype before initilization: "+typeof isWorking);
+isWorking=true;
+console.log("isWorking datatype After initilization: "+ typeof isWorking);
+var totalCount=107;
+console.log("totalCount datatype: "+typeof totalCount);
