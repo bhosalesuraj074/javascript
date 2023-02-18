80cfb8615f9aabfa934e7e737839d9636c15285b
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-02-18 17:36:49 +0530

    Create 01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,20 @@
+// variable declaration
+var myFullName;
+
+//printing the value on console
+console.log(myFullName);
+
+//initilization 
+myFullName ="Suraj J. Bhosale"
+console.log(myFullName);
+
+// updating  myFullName
+myFullName ="Suraj Jalidnar Bhosale";
+console.log(myFullName)
+
+// other operation
+var myLove="I Love JavaScript";
+console.log(myLove);
+
+var firstName="Suraj", lastName="Bhosale", age=24, address="Sangola, Khavaspur", pincode=1234567, gender="male", isMarried="Single";
+console.log(firstName,lastName,age,address,pincode,gender,isMarried);
