6cee0da687edac8b2db483e376d0d1a0afb2c9fd
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-02-18 18:09:29 +0530

    Update 01_swapvar_assign.js

--- 01_swapvar_assign.js
+++ 01_swapvar_assign.js
@@ -1,3 +1,5 @@
+console.log("-----------------------------------------");
+//swapping  two variables
 var v=18;
 var r=45;
 v=v+r;
@@ -5,3 +7,31 @@ r=v-r;
 v=v-r;
 console.log(v);
 console.log(r);
+
+console.log("-----------------------------------------");
+//swapping string variables 
+var sweety="Swetty";
+var cutie="Cutie";
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("Swapping the string");
+console.log(sweety);
+console.log(cutie);
+
+// swapping three values
+console.log("-----------------------------------------");
+var num1=100;
+var num2=200;
+var num3=300;
+var t=num1;
+num1=num2;
+num2=num3;
+num3=t;
+console.log("swapping three values");
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+
+
