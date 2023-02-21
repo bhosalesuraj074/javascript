bda518fcc9af356bbd7aed87f11ade27b85099c8
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-02-21 16:26:57 +0530

    Update 04_string_3.js

--- 04_string_3.js
+++ 04_string_3.js
@@ -1,1 +1,11 @@
-console.log("My dream company is Google");
+function stringTemplateAssign()
+{
+  console.log("My dream company is 'Google'.");
+  var hobby1 ,hobby2,hobby3;
+  hobby1="Traveling";
+  hobby2="Playing Cricket";
+  hobby3="Learning new Thinks";
+  console.log("My hobbies includes "+hobby1+", "+hobby2+", "+" & "+hobby3);
+  console.log("Concated String as below :\n"+hobby1+hobby2+hobby3);
+}
+stringTemplateAssign()
