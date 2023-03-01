f698ac4158e7448228a6afe58c814889dbb7273c
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-03-01 10:34:05 +0530

    Update 07_loop_for.js

--- 07_loop_for.js
+++ 07_loop_for.js
@@ -3,43 +3,48 @@ for (let index = 5; index <=15; index++) {
     console.log(index);
     
 }
-// Task 1: print 50 to 40
+// Task 2: print 50 to 40
 console.log("-----------------------------------")
 for (let index = 50; index >=40; index--) {
     console.log(index);
     
 }
-// Task 1: print odd number 1 to 15
+// Task 3: print odd number 1 to 15
 console.log("-----------------------------------")
 for (let index = 1; index <= 15; index +=2) {
     console.log(index);
     
 }
-// Task 1: print even number 1 to 10
+// Task 4: print even number 1 to 10
 console.log("-----------------------------------")
 for (let index = 2; index <=20; index += 2) {
     console.log(index);
     
 }
-// Task 1: print 1 to 10
+// Task 5: print 5 table
 console.log("-----------------------------------")
 for (let index = 1; index <=10; index++) {
     console.log(index*5);
     
 }
-// Task 1: print 1 to 10
+// Task 6: print 10 table
 console.log("-----------------------------------")
 for (let index = 1; index <=10; index++) {
     console.log(index*10);
     
 }
-// Task 1: print 1 to 10
-console.log("-----------------------------------")
-for (let index = 1; index <=10; index++) {
-    console.log(index*5);   
-}
+
+// Task 7: print
 console.log("-----------------------------------")
 for (let index = 1; index <=10; index++) {
     console.log(index*5);
     
 }
+
+console.log("-----------------------------------")
+//8. print table 
+for(let i=10;i>0;i--)
+{
+    console.log(i*10);
+}
+console.log("-----------------------------------") 
