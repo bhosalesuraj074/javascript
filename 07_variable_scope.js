800daf7f2dd602324d6edcda54a5299012a16ac5
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-03-03 10:22:42 +0530

    scope of the variable like var, let & const

---
+++ 07_variable_scope.js
@@ -0,0 +1,13 @@
+var firstName="Suraj";
+console.log(firstName);
+var firstName;
+console.log(firstName);
+let lastName="Bhosale";
+lastName="Boss";
+console.log(lastName);
+//let lastName ="King";
+if(true)
+{
+    var a="Bs";
+}
+console.log(a);
