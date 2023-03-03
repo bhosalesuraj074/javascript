4f3e49b02ff2d4375a7eeccad14ad87916af9bc7
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-03-03 16:10:25 +0530

    factorial of the string

--- 06_factorial_B.js
+++ 06_factorial_B.js
@@ -1,17 +1,34 @@
-function factorialOfWordCount(str)
+function factorialOfWordCount(word)
 {
-    
-    if(str == null || isNaN(str) || typeof str !== typeof "string")
+     var result =1 ;
+       //var len = word.length;
+    if(word == null || word == undefined || typeof word == typeof isNaN(word))
     {
-        return `You have entered ${str}. please enter the valid input`;
+        return `You have entered '${word}'. please enter the valid input`;
     }
-    var num = str.length;
+    let num = word.split(" ").length;
+    if(word.length > 0)
+    {
     for (let index = 1; index <= num; index++) 
     {
         result = result * index;
     }
-    return `The factorial of ${str} is '${result}'`;
+    return `string: '${word}', length of the string is '${num}' and factorial of the string is '${result}'`;
+    }
+    else{
+        return `string: "${word}", Length of the string <= 0`;
+    }
 }
- console.log(factorialOfWordCount(null))
- console.log(factorialOfWordCount(NaN))
- console.log(factorialOfWordCount("Revision is the mother of success"));
+console.log("Task 2. Finding factorial of the string length");
+console.log("-----------------------------------------------------------------------------------------------------------------------------");
+console.log(factorialOfWordCount("Revision is the mother of success"));
+console.log("-----------------------------------------------------------------------------------------------------------------------------");
+console.log(factorialOfWordCount("we never fail, we alway learn"));
+console.log("-----------------------------------------------------------------------------------------------------------------------------");
+console.error(factorialOfWordCount(null));
+console.log("-----------------------------------------------------------------------------------------------------------------------------");
+console.error(factorialOfWordCount(""));
+console.log("-----------------------------------------------------------------------------------------------------------------------------");
+console.log(factorialOfWordCount("Suraj Jalindar Bhosale"));
+console.log("-----------------------------------------------------------------------------------------------------------------------------");
+
