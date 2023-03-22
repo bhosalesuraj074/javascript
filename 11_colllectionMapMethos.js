62035dbe3b078fceeaca34867937cb084a31d29f
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-03-22 11:07:15 +0530

    collection

---
+++ 11_colllectionMapMethos.js
@@ -0,0 +1,23 @@
+const array = [3, 4, 5, 6, 1];
+           // [5, 6, 7, 8, 3];
+
+const arrayTransformed = [];
+array.forEach( (currentValue) => {
+    arrayTransformed.push(currentValue+2)
+} );
+console.log(arrayTransformed);
+
+console.log("Using map()");
+const arrayNew = array.map( (element) => {
+   return element + 2;
+} )
+//console.log(array);
+console.log(arrayNew);
+
+const arrayOfNames = ["Rupesh Dhumal", "Rutuja Pawar", "Nayan", "Pralhad"];
+const arrayElementSize= [];
+arrayOfNames.map((element)=>
+{
+   arrayElementSize.push(element.length);
+})
+console.log(arrayElementSize);
