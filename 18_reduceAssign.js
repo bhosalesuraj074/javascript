2840bcf948d669182b3ab9693d4cbceabf9a942c
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2024-01-25 16:02:59 +0530

    reduce Assignment

--- 18_reduceAssign.js
+++ 18_reduceAssign.js
@@ -15,3 +15,46 @@ const emp_sonali = new Employee(66, 'Sonali', 'Finance', 45000, 'Infy');
 const emp_monika =new Employee(77, 'monika', 'IT', 40000, 'Wipro');
 const emp_viny =new Employee(88, 'Vinayak', 'IT', 75000, 'TCS');
 const emp_mahi =new Employee(99 , 'Mahesh', 'HR', 85000,'Infy');
+
+
+let arrayEmps = [emp_anil, emp_radha, emp_rishi, emp_sonali , emp_monika, emp_viny, emp_mahi];
+console.log('-------------------Wipro Employees ------------------');
+let wipro_emp =arrayEmps.filter((emp)=>{
+    return emp.emp_company == 'Wipro';
+
+})
+console.log(wipro_emp);
+
+console.log('-------------------IT & HR Employees ------------------');
+let it_hr_emp = arrayEmps.filter(emp=>{
+return emp.emp_dept == 'IT' || emp.emp_dept == 'HR';
+})
+console.log(it_hr_emp);
+
+console.log('-------------------Employee Id > 50 ------------------');
+let emp_50 = arrayEmps.filter(emp=>{
+return emp.emp_id > 50;
+})
+console.log(emp_50);
+
+console.log('-------------------Employee name startwith A V M ------------------');
+let name_AVM = arrayEmps.filter(emp =>{
+    return emp.emp_name.startsWith('A') || emp.emp_name.startsWith('V') || emp.emp_name.startsWith('M')
+})
+console.log(name_AVM);
+
+console.log('-------------------Avrage of the all deparatment ------------------');
+let dept_avg =  arrayEmps.reduce((startWith , emp)=>
+{
+
+    return startWith +=emp.emp_salary;
+
+}, 0);
+
+console.log(dept_avg/ arrayEmps.length);
+
+console.log('-------------------Avrage of the IT deparatment ------------------');
+let IT_avg  = arrayEmps.filter(emp => emp.emp_dept == 'IT');
+let avg = IT_avg.reduce(startemp =>{
+    return
+})
